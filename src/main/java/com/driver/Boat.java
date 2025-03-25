7383dd584a5ef053e3908bacf9a214be199319ae
kunalt1204 <me.kunaltrivedi97@gmail.com>
2025-03-25 22:14:55 +0530

    ok

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,35 +1,29 @@
 package com.driver;
 
 public class Boat implements WaterVehicle {
-    // Private fields for encapsulation
-    private String vehicleName;
-    private int vehicleCapacity;
+    private String name;
+    private int capacity;
 
-    // Constructor to initialize the boat's properties
-    public Boat(String vehicleName, int vehicleCapacity) {
-        this.vehicleName = vehicleName;
-        this.vehicleCapacity = vehicleCapacity;
+    public Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
     }
 
-    // Setter for vehicle name
-    public void setVehicleName(String vehicleName) {
-        this.vehicleName = vehicleName;
+    public String getName(){
+        return this.name;
     }
 
-    // Setter for vehicle capacity
-    public void setVehicleCapacity(int vehicleCapacity) {
-        this.vehicleCapacity = vehicleCapacity;
+    public int getCapacity(){
+        return this.capacity;
     }
 
-    // Implementation of getVehicleName from WaterVehicle interface
     @Override
     public String getVehicleName() {
-        return this.vehicleName;
+        return name;
     }
 
-    // Implementation of getVehicleCapacity from WaterVehicle interface
     @Override
     public int getVehicleCapacity() {
-        return this.vehicleCapacity;
+        return capacity;
     }
 }
